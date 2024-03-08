6a8351a7a3bf0b03a6e0b44bd7e0cb53dc490ad5
aort <artashesano@gmail.com>
2024-03-08 18:37:58 +0400

    client: Add movie details page to router

--- client/src/renderer/src/router.tsx
+++ client/src/renderer/src/router.tsx
@@ -1,11 +1,24 @@
-import Home from '@renderer/pages/home'
-import { createBrowserRouter } from 'react-router-dom'
 import { movieStore } from '@renderer/lib/movie-store'
+import Home from '@renderer/pages/home'
+import MovieDetails from '@renderer/pages/movie-details'
+import { IMovieDetails } from 'omdb-sdk'
+import { Navigate, createBrowserRouter } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
     path: '/',
+    element: <Navigate to="/movies" />
+  },
+  {
+    path: '/movies',
     element: <Home movieStore={movieStore} />
+  },
+  {
+    path: '/movies/:id',
+    element: <MovieDetails />,
+    loader: async ({ params }): Promise<IMovieDetails | null> => {
+      return await movieStore.getMovieDetailsById(params.id!)
+    }
   }
 ])
 
