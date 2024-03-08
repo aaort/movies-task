7cbe48c3c9a001820c7806493472f32ca702b237
aort <artashesano@gmail.com>
2024-03-09 00:06:54 +0400

    client: ErrorAlert now takes optional title and description props

--- client/src/renderer/src/components/custom/error-alert.tsx
+++ client/src/renderer/src/components/custom/error-alert.tsx
@@ -1,18 +1,28 @@
 import { CodeIcon } from '@radix-ui/react-icons'
 import { Alert, AlertDescription, AlertTitle } from '@renderer/components/ui/alert'
-import { ReactNode } from 'react'
+import { FC } from 'react'
 import ReloadButton from './reload-button'
 
-const ErrorAlert = (): ReactNode => {
+interface IErrorAlert {
+  title?: string
+  description?: string
+}
+
+const defaultTitle = 'Something went wrong'
+const defaultDescription = 'Please try again later, or try to reload the page'
+
+const ErrorAlert: FC<IErrorAlert> = (props) => {
+  const { title = defaultTitle, description = defaultDescription } = props
+
   return (
     <Alert>
       <CodeIcon />
 
-      <AlertTitle>Something went wrong</AlertTitle>
+      <AlertTitle>{title}</AlertTitle>
 
       <AlertDescription>
         <div className="flex flex-row justify-between">
-          <span>Please try again later, or try to reload the page</span>
+          <span>{description}</span>
 
           <ReloadButton />
         </div>
