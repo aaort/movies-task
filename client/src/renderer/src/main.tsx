afd5a583598bba1c94d38604370e30420dcf3f55
aort <artashesano@gmail.com>
2024-03-09 19:18:14 +0400

    Use import path alias in main.tsx

--- client/src/renderer/src/main.tsx
+++ client/src/renderer/src/main.tsx
@@ -1,10 +1,10 @@
 import './globals.css'
 
+import { ThemeProvider } from '@renderer/components/custom/theme-provider'
 import { router } from '@renderer/router'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { ThemeProvider } from './components/custom/theme-provider'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
