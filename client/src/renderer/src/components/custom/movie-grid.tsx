b1aff3537b5718dc96185a6569356de7a4cb6c47
aort <artashesano@gmail.com>
2024-03-09 19:20:58 +0400

    Add MovieGrid with loading state management

---
+++ client/src/renderer/src/components/custom/movie-grid.tsx
@@ -0,0 +1,29 @@
+import MovieCard from '@renderer/components/custom/movie-card'
+import Skeleton from '@renderer/components/ui/skeleton'
+import { IMovie } from 'omdb-sdk'
+import { FC } from 'react'
+
+const skeletons = Array.from({ length: 10 }, (_, i) => i + 1)
+
+interface MovieGrid {
+  movies: IMovie[]
+  loading: boolean
+}
+
+const MovieGrid: FC<MovieGrid> = (props) => {
+  const { loading, movies } = props
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6">
+      {(loading ? skeletons : movies).map((item) =>
+        loading ? (
+          <Skeleton key={item} className="h-[400px]" />
+        ) : (
+          <MovieCard key={item.imdbID} movie={item} />
+        )
+      )}
+    </div>
+  )
+}
+
+export default MovieGrid
