1585fd0e674a026aecd602371f38d6a8e53984ae
aort <artashesano@gmail.com>
2024-03-10 17:01:30 +0400

    Add MovieDetailsErrorBoundary

---
+++ client/src/renderer/src/components/custom/boundaries/movie-details-error.tsx
@@ -0,0 +1,19 @@
+import ErrorAlert, { IErrorAlertProps } from '@renderer/components/custom/error-alert'
+import { ReactNode } from 'react'
+import { useParams, useRouteError } from 'react-router-dom'
+
+const errorAlertTexts = (id?: string): IErrorAlertProps => ({
+  title: `Unable to find the movie with the id: ${id}`,
+  description: `Try reloading the page, if issue still persists, navigate home`
+})
+
+function MovieDetailsErrorBoundary(): ReactNode {
+  const params = useParams()
+  const error = useRouteError() as Record<string, string>
+
+  console.error('this is the error', error)
+
+  return <ErrorAlert {...errorAlertTexts(params.id)} />
+}
+
+export default MovieDetailsErrorBoundary
