5e43faa2ea8d7129ac100226c89fd9ded64859fc
aort <artashesano@gmail.com>
2024-03-10 17:34:55 +0400

    Remove "base" option in vite.config./js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   root: './src/renderer',
-  base: './src/renderer',
   resolve: {
     alias: {
       '@renderer': resolve('./src/renderer/src')
